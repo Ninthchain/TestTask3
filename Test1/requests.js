c6bf5166848c56d3f9e21fbc006853d0e13fc047
Ninthchain <73429297+Ninthchain@users.noreply.github.com>
2023-03-18 21:30:28 +0300

    'tempCommit'

---
+++ Test1/requests.js
@@ -0,0 +1,27 @@
+const { AxiosHeaders } = require('axios')
+
+const axios = require('axios').default
+
+
+// Async GET|POST req 
+
+var asyncReq = (async () => {
+    let getResponse = await axios.get("https://httpbin.org/get")
+    console.log(getResponse.data)
+
+    let postResponse = await axios.post("https://httpbin.org/post", {}, {
+        headers: {
+            Accept: "application/json"
+        }
+    })
+    console.log(postResponse.data)
+})
+
+
+asyncReq()
+
+// Sync req
+
+var syncReq = (() => {
+    let  = axios.get()
+})
